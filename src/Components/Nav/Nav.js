745fa62ebbc16ad04a73fa523543e70fb08f7c06
JNKarif <arif.rahman041@gmail.com>
2022-09-29 06:02:10 +0600

    first commit

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fa-book - open - reader } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import './Nav.css'
+
+
+
+const Nav = () => {
+    return (
+        <div>
+
+            <nav>
+                <FontAwesomeIcon icon={fa - book - open - reader}>
+
+                </FontAwesomeIcon>
+
+                <h1>LEARN & RUN</h1>
+
+            </nav>
+
+        </div>
+    );
+};
+
+export default Nav;
